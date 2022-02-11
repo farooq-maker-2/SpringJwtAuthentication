6cb704bc57173d0e442d84f72b7fbf7e7a865d74
Muhammad farooq <>
2022-02-11 11:14:36 +0500

    refactoring

--- src/main/resources/db/migration/V1__admin_init.sql
+++ src/main/resources/db/migration/V1__admin_init.sql
@@ -8,7 +8,3 @@ CREATE TABLE `admins` (
                           `status` varchar(255) DEFAULT NULL,
                           PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-INSERT INTO admins (first_name, last_name, email, password, role, status)
-VALUES ('Muhammad', 'Farooq', 'farooq.admin@gmail.com', '$2a$10$Tic/wlaslKB5KlEVnPuKW.3/zXG/EKotV9XGDlMfpjjB/M6.UEEkC',
-        'admin', 'activated');
